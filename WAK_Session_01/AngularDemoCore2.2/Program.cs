3de6363767910252833f91c50b6b59d120a0b6b0
Nahid Hasan <nahid.hasan@capgemini.com>
2019-07-16 14:29:55 +0200

    Environment variable support added

--- WAK_Session_01/AngularDemoCore2.2/Program.cs
+++ WAK_Session_01/AngularDemoCore2.2/Program.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace AngularDemoCore2._2
 {
@@ -19,6 +13,10 @@ namespace AngularDemoCore2._2
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                   .ConfigureAppConfiguration(config =>
+                   {
+                       config.AddEnvironmentVariables("AzureDBConnectionSettings_");
+                   })
+                   .UseStartup<Startup>();
     }
 }
